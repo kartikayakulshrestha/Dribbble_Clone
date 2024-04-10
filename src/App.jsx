7f1be42c253fa8fca2c681119fd9b70db74c46dd
kartikayakulshrestha <kartikaya.kul0908@gmail.com>
2024-04-10 11:11:54 +0530

    Solved an issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,29 @@
 
 import "./output.css"
 import './App.css'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Login from "./pages/Login.jsx"
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+import Notfound from './pages/notfound.jsx'
+import Step2 from './pages/Step2.jsx'
+import Verify from './pages/Verif.jsx'
+import Step3 from './pages/Step3.jsx'
+import Home from "./pages/Home.jsx"
 function App() {
   
 
   return (
-    <div>
-      <Navbar />
-      <div className=" text-5xl font-bold text-center ">
-      <a href='/signup'>login Please!!! Click me</a>
-      </div>
-      
-      <Footer />
-    </div>
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/signup" element={<Login />}></Route>
+      <Route path="/signup/step2" element={<Step2 />}></Route>
+      <Route path="/signup/step3" element={<Step3 />}></Route>
+      <Route path="/verify" element={<Verify />}></Route>
+      <Route path="/*" element={<Notfound />}></Route>
+    </Routes>
+  </BrowserRouter>
+    
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <a href="/signup">Click me</a>
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
